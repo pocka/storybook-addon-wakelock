52d0d453be65bf289ed3b476bd2acf6621664392
Shota Fuji <pockawoooh@gmail.com>
2022-08-14 09:51:51 +0000

    chore: Add unit testing

---
+++ src/hello.spec.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest"
+
+import hello from "./hello"
+
+describe("Sample Test", () => {
+  it("Should pass", () => {
+    expect(hello).toBe("BAR!")
+  })
+})
