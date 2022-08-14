e0c652f563d835db6a17c749fe5db914dcb392a6
Shota Fuji <pockawoooh@gmail.com>
2022-08-14 09:56:27 +0000

    style: npm run fmt

--- src/hello.spec.ts
+++ src/hello.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it } from "vitest";
 
-import hello from "./hello"
+import hello from "./hello";
 
 describe("Sample Test", () => {
   it("Should pass", () => {
-    expect(hello).toBe("BAR!")
-  })
-})
+    expect(hello).toBe("BAR!");
+  });
+});
